795b68ce87d936255b247f8733701616a1ac830c
PNorvaisas <povilas.norvaisas@gmail.com>
2015-05-22 20:58:30 +0100

    Filtering

--- PlateExtract.py
+++ PlateExtract.py
@@ -27,6 +27,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from pylab import *
 
+import scipy.signal as sig
 from scipy.fftpack import rfft, irfft, fftfreq
 from collections import OrderedDict
 from collections import defaultdict
@@ -52,6 +53,8 @@ Arguments:
 	-c <file>    Name of control file
 	-e <file>    Name of experiment
 	-d <file>    CSV file with data labels
+	-f <filter>  wiener,butter
+	-o <dir>     directory to write output to
 
 Options:
 
@@ -71,7 +74,10 @@ optionsset='''
 Options:
 <--------------------------------------------->
     Control:	%(cfile)s
-    FFT-f:	%(freq)s
+ Experiment:	%(efile)s
+	 DB:	%(dfile)s
+     Filter:	%(filterf)s
+        Out:	%(odir)s
 <--------------------------------------------->
 	'''
 
@@ -82,13 +88,15 @@ def main(argv=None):
 	cfile=""
 	efile=""
 	dfile=""
-	freq=0.0005
+	msize=20
 	mode="Zaslaver"
+	filterf=''
+	odir='Output'
 	if argv is None:
 		argv = sys.argv
 	try:
 		try:
-			opts, args = getopt.getopt(argv[1:], "hc:e:f:d:", ["help"])
+			opts, args = getopt.getopt(argv[1:], "hc:e:f:d:o:", ["help"])
 		except getopt.error, msg:
 			raise Usage(msg)
 
@@ -103,8 +111,10 @@ def main(argv=None):
 				efile=value
 			if option in ("-d", "--db"):
 				dfile=value
-			if option in ("-f", "--freq"):
-				freq=float(value)
+			if option in ("-f", "--filter"):
+				filterf=value
+			if option in ("-o", "--out"):
+				odir=value
 	
 	
 		for argument in args:		
@@ -135,6 +145,13 @@ def main(argv=None):
 			dpath, dname, dtype = filename(dfile)			
 		else:
 			raise Exception("No experiment file specified.")
+		if filterf!='' and filterf in ['wiener','butter']:
+			print "{} filter selected".format(filterf)
+		else:
+			print "Unknown filter {}, not using.".format(filterf)
+			filterf=''
+			
+		
 
 
 		
@@ -159,17 +176,29 @@ def main(argv=None):
 	#ilist=genlist(ifile)
 	ilist=[cfile,efile]	
 	data=collect(ilist)
-	data,figures=analyze(data,waves,mode,freq)
+	data=analyze(data,waves,filterf)
 	plate=cfile.split('_')[1]
-	#dirn=dircheck('UAL')	
+	dirn=dircheck(odir)
+	dirh=dircheck(odir+'/Huge')
+	zoom=['B1','B2','B3']
 	print plate
-	for nm in figures:
-		print "Plotting %(nm)s..." %vars()
-		plt, plots=plot_2D(plate+"-"+nm,data['Control'][nm],data['Experiment'][nm],data['Control']['Time'],data['Control']['Labels'],data['Control']['Wells'],genes[plate])
-		data['Joint'][nm]=plt
-		plt.savefig('{}/{}.pdf'.format('UAL',plate+'_'+nm))
+	for fg in data['Control']['Figures']:
+		print "Plotting %(fg)s..." %vars()
+		if 'dt' in fg:
+			plt, plots=plot_2D(plate+"-"+fg,data['Control'][fg],data['Experiment'][fg],data['Control']['Time_dt'],data['Control']['Labels'],data['Control']['Wells'],genes[plate])
+		else:
+			plt, plots=plot_2D(plate+"-"+fg,data['Control'][fg],data['Experiment'][fg],data['Control']['Time'],data['Control']['Labels'],data['Control']['Wells'],genes[plate])
+		data['Joint'][fg]=plt
+		plt.savefig('{}/{}.pdf'.format(dirn,plate+'_'+fg))
+
+		if 'dt' in fg:
+			plotsh=plothuge(plate+'_'+fg,data['Control'][fg],data['Experiment'][fg],data['Control']['Time_dt'],zoom,genes[plate])
+		else:
+			plotsh=plothuge(plate+'_'+fg,data['Control'][fg],data['Experiment'][fg],data['Control']['Time'],zoom,genes[plate])
+		for l in zoom:
+			plotsh[l].savefig('{}/{}.pdf'.format(dirh,plate+'_'+fg+'_'+l))
 	
-	
+
 	
 
 	
@@ -189,7 +218,10 @@ def plot_2D(title,datac,datae,time,labels,plate_size,genes):
 	#Need to check for both datasets
 	totalmaxc=round_to(max([max(datac[l]) for l in labels]),0.1)
 	totalmaxe=round_to(max([max(datae[l]) for l in labels]),0.1)
+	totalminc=round_to(min([min(datac[l]) for l in labels]),0.1)
+	totalmine=round_to(min([min(datae[l]) for l in labels]),0.1)
 	totalmax=max([totalmaxc,totalmaxe])
+	totalmin=min([totalminc,totalmine])
 	if "dt" in title:
 		ymin=-totalmax
 	else:
@@ -228,9 +260,9 @@ def plot_2D(title,datac,datae,time,labels,plate_size,genes):
 		if row==1:
 			plt.title(col)
 
-		plt.xticks(np.linspace(0, max(x), 4),['']+list(np.linspace(0, max(x), 4).astype(int)[1:]), rotation='vertical')	
-		plt.yticks(np.linspace(ymin, totalmax, 5),['']+list(np.around(np.linspace(ymin, totalmax, 5),2)[1:]))
-		plt.ylim([ymin,totalmax])
+		plt.xticks(np.linspace(0, max(x), 2),['']+list(np.linspace(0, max(x), 2).astype(int)[1:]), rotation='vertical')	
+		plt.yticks(np.linspace(ymin, totalmax, 2),['']+list(np.around(np.linspace(totalmin, totalmax, 2),2)[1:]))
+		plt.ylim([totalmin,totalmax])
 		plots[l].text(0.1, 0.8, genes[l]['Gene'], fontsize=10, transform=plots[l].transAxes)
 		#exec(l+"='plt.subplot(8,12,{},sharex={},sharey={})'".format(v,,sh_y))
 
@@ -240,30 +272,80 @@ def plot_2D(title,datac,datae,time,labels,plate_size,genes):
 
 	return plt, plots
 
-def analyze(data,waves,mode,freq):
+def Wiener(y, n):
+	wi = sig.wiener(y, mysize=n)
+	return wi
+
+def Butter(x, y, nyf, par1, par2):
+	b, a = sig.butter(par1, par2/nyf)
+	fl = sig.filtfilt(b, a, y)
+	return fl
+
+def plothuge(title,datac,datae,time,labels,genes):
+	plots={}
+	#Need to check for both datasets
+	totalmaxc=round_to(max([max(datac[l]) for l in labels]),0.1)
+	totalmaxe=round_to(max([max(datae[l]) for l in labels]),0.1)
+	totalminc=round_to(min([min(datac[l]) for l in labels]),0.1)
+	totalmine=round_to(min([min(datae[l]) for l in labels]),0.1)
+	totalmax=max([totalmaxc,totalmaxe])
+	totalmin=min([totalminc,totalmine])
+	x=time/3600
+	for i,l in enumerate(labels):	
+		#print l,i
+		yc=datac[l]
+		ye=datae[l]
+		fig=plt.figure(i)
+		fig.suptitle(title+'_'+l+'-'+genes[l]['Gene'])
+		plt.plot(x,yc,'r-',x,ye,'b-')
+		#plt.text(0.1, 0.8, genes[l]['Gene'], fontsize=12, transform=transAxes)
+		plots[l]=fig
+		#plt.show()
+	return plots
+
+
+def analyze(data,waves,filterf):
+	msize=20
+	par1=4
+	par2=1.57
 	for tp in data.keys():		
-		if mode=='Zaslaver' and len([nm for nm in waves if nm in data[tp]['Waves']])==2:
+		if len([nm for nm in waves if nm in data[tp]['Waves']])==2:
 			time=data[tp]['Time']
 			dt=data[tp]['Time'][1]-data[tp]['Time'][0]
+			npts=len(time)
+			dtm=time[-1]/npts
+			nyf=0.5/dtm
 			data[tp]['Time_dt']=(time+dt/2)[:-1]
 			Ufluor=data[tp]['535nm']['C10']/data[tp]['600nm']['C10']
 			for well in data[tp]['Labels']:
-				data[tp]['600nm_zero'][well]=data[tp]['600nm'][well]-data[tp]['600nm'][well+'_min']
-				#data[tp]['600nm_norm'][well]=data[tp]['600nm_zero'][well]/(data[tp]['600nm'][well+'_max']-data[tp]['600nm'][well+'_min'])
+				growth=data[tp]['600nm'][well]-data[tp]['600nm'][well+'_min']
 				fluor=data[tp]['535nm'][well]/data[tp]['600nm'][well]
+
+
+				if filterf=='wiener':
+					growth=Wiener(growth-growth[0],msize)
+					fluor=Wiener(fluor-fluor[0],msize)
+					fluor_norm=fluor-Ufluor
+					fluor_norm=Wiener(fluor_norm-fluor_norm[0],msize)
+				elif filterf=='butter':
+					growth=Butter(time,growth-growth[0],nyf,par1,par2)
+					fluor=Butter(time,fluor-fluor[0],nyf,par1,par2)
+					fluor_norm=fluor-Ufluor
+					fluor_norm=Butter(time,fluor_norm-fluor_norm[0],nyf,par1,par2)
+				else:
+					fluor_norm=fluor-Ufluor
+								
+				growth=growth-min(growth)
+				fluor=fluor-min(fluor)
+				fluor_norm=fluor_norm-min(fluor_norm)
 				
-				#fluor_lp=fft(fluor,time/3600,freq)
-				fluor_min=min(fluor)
+
 				fluor_max=max(fluor)
-				fluor_norm=fluor-Ufluor
+				fluor_min=min(fluor)
 				
+				data[tp]['Growth'][well]=growth
 				data[tp]['Fluorescence'][well]=fluor
 				data[tp]['Fluorescence_norm'][well]=fluor_norm
-				#data[tp]['Fluor_lp'][well]=fluor_lp
-				#fluor_lp_min=min(fluor_lp)
-				#fluor_lp_max=max(fluor_lp)
-
-				#data[tp]['Fluor_norm'][well]=(data[tp]['Fluor'][well]-fluor_min)/(fluor_max-fluor_min)
 
 				data[tp]['Fluor'][well+'_max']=fluor_max
 				data[tp]['Fluor'][well+'_min']=fluor_min
@@ -274,16 +356,15 @@ def analyze(data,waves,mode,freq):
 
 				
 
-				#data[tp]['Fluor_dt'][well]=np.diff(fluor)/dt
+				data[tp]['Fluorescence_dt'][well]=np.diff(fluor_norm)/dt
 				#data[tp]['Fluor_lp_dt'][well]=np.diff(fluor_lp)/dt
 				
 				
 					
 
-			data[tp]['Figures']=data[tp]['Figures']+['600nm_zero','Fluorescence', 'Fluorescence_norm'] #'Fluor_norm','Fluor_lp','Fluor_lp_norm','Fluor_dt','Fluor_lp_dt'
-		if mode=='Biolog' and len([nm for nm in waves if nm in data[tp]['Waves']])==2:
-			print "Not yet developed"
-	return data, ['600nm_zero','Fluorescence','Fluorescence_norm']
+			data[tp]['Figures']=data[tp]['Figures']+['Growth','Fluorescence', 'Fluorescence_norm','Fluorescence_dt'] #'Fluor_norm','Fluor_lp','Fluor_lp_norm','Fluor_dt','Fluor_lp_dt'
+
+	return data
 
 def csvreader(dfile):
 	genes=NestedDict()
@@ -344,53 +425,7 @@ def collect(ilist):
 		#print timemax_h, timestep
 	return data
 
-def plot_fft(signal,time,freq):
 
-	W = fftfreq(signal.size, d=time[1]-time[0])
-	f_signal = rfft(signal)
-
-	# If our original signal time was in seconds, this is now in Hz    
-	cut_f_signal = f_signal.copy()
-	for i,f in enumerate(W):
-		#print i, f
-		if f>freq or f<-freq:
-			cut_f_signal[i]=0
-	#cut_f_signal[(W>freq)] = 0
-	print "W"
-	#print W
-	print f_signal
-	cut_signal = irfft(cut_f_signal)
-	print cut_f_signal
-	
-
-	plt.subplot(221)
-	plt.plot(time,signal)
-	plt.subplot(222)
-	plt.plot(W,f_signal)
-	#plt.xlim(0,0.002)
-	plt.subplot(223)
-	plt.plot(W,cut_f_signal)
-	#plt.xlim(0,0.002)
-	plt.subplot(224)
-	plt.plot(time,cut_signal)
-	plt.show()
-
-def fft(signal,time,freq):
-
-	W = fftfreq(signal.size, d=time[1]-time[0])
-	f_signal = rfft(signal)
-
-	# If our original signal time was in seconds, this is now in Hz    
-	cut_f_signal = f_signal.copy()
-	#cut_f_signal[0 for s,f in IT.izip(cut_f_signal,W) if f>freq else s ]
-	for i,f in enumerate(W):
-		#print i, f
-		if f>freq or f<-freq:
-			cut_f_signal[i]=0
-
-	cut_signal = irfft(cut_f_signal)
-
-	return cut_signal
 
 def genlist(ifile):
 	#Generates list of input files, checks their existance
